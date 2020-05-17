a225e38ea0dfc0de057b3b8c527a3af2c662febe
Khalmurad <46134234+khalmurad@users.noreply.github.com>
2020-05-17 14:41:34 +0500

    Update routes.php
    routes.php file

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'news' => 'news/index', //NewsController-dagi => actionIndex
-    'products' => 'product/list', //ProductController-dagi => actionList
+    'news' => 'news/index', //NewsController-dagi => actionIndex ***** in NewsController => actionIndex
+    'products' => 'product/list', //ProductController-dagi => actionList ***** in ProductController => actionList
 ];
